8e7133cea5bf699a0a4b7650cd472438bb281850
Arnaud Magnan <arnaud@MacBook-Air-de-Arnaud.local>
2019-01-11 13:05:15 -0800

    day04

---
+++ day04/ex01/create.php
@@ -0,0 +1,46 @@
+<?php
+
+function ft_exit($str)
+{
+	echo "$str\n";
+	exit();
+}
+
+$login = $_POST['login'];
+$passwd = $_POST['passwd'];
+if ($passwd != "" && $_POST['submit'] == "OK")
+{
+	if (($file_serialized = file_get_contents("../private/passwd")) != "")
+	{
+		$file_unserialized = unserialize($file_serialized);
+		foreach ($file_unserialized as $x)
+		{
+			foreach ($x as $key => $value)
+			{
+				if ($value == $login)
+					ft_exit("ERROR\n");
+			}
+		}
+		$hashed = hash("whirlpool", $passwd);
+		array_push($file_unserialized, array("login" => $login, "passwd" => $hashed));
+		$data_serialized = serialize($file_unserialized);
+		file_put_contents("../private/passwd", $data_serialized);
+		echo "OK\n";
+	}
+	else
+	{
+		$hashed_password = hash("whirlpool", $passwd);
+		$data = array(
+			array(
+				"login" => $login,
+				"passwd" => $hashed_password;
+			)
+		);
+		$data_serialized = serialize($data);
+		file_put_contents("../private/passwd", $data_serialized);
+		echo "OK\n";
+	}
+}
+else
+	echo "ERROR\n";
+?>
